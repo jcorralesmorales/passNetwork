a830844b7855bc13e61037d9c36476484441aaa2
jcorralesmorales <65259155+jcorralesmorales@users.noreply.github.com>
2023-03-08 13:11:35 -0600

    Update streamlit_example.py

--- streamlit_example.py
+++ streamlit_example.py
@@ -36,7 +36,7 @@ st.write("""
 # Demo
 """)
 parent_path = pathlib.Path(__file__).parent.parent.resolve()
-data_path = os.path.join(parent_path, "data")
+data_path = os.path.join(parent_path, "passNetwork/data")
 onlyfiles = [f for f in listdir(data_path) if isfile(join(data_path, f))]
 option = st.sidebar.selectbox('Pick a dataset', onlyfiles)
 file_location=os.path.join(data_path, option)
